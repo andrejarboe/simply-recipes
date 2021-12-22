8b969713d2e0fa16864546f2bae81c9a79a8de9e
Andre Jarboe <andrejarboe@gmail.com>
2021-12-21 20:57:40 -0500

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,10 @@ import MainLayout from "../components/Layouts/MainLayout"
 export default function Error() {
   return (
     <MainLayout>
-      <h1>404</h1>
-      <h1>Error</h1>
+      <div className="container text-teal-900 mx-auto min-h-full flex flex-col justify-center items-center">
+      <h1 className="mt-40 text-7xl font-semibold">404</h1>
+      <h1 className="text-5xl font-semibold">Page Not Found</h1>
+      </div>
     </MainLayout>
   )
 }
