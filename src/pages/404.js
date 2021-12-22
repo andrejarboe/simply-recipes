e17f715edc71b5479d2e7e2a13c10d2df1953bfc
Andre Jarboe <andrejarboe@gmail.com>
2021-12-21 20:38:25 -0500

    up date 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react"
+import MainLayout from "../components/Layouts/MainLayout"
 
 export default function Error() {
-    return (
-        <div>
-            404
-            <a href="/">Go home</a>
-        </div>
-    )
+  return (
+    <MainLayout>
+      <h1>404</h1>
+      <h1>Error</h1>
+    </MainLayout>
+  )
 }
