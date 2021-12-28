d8850aa5aacb8e19a7d0258c3778e20fadce29bd
Andre Jarboe <andrejarboe@gmail.com>
2021-12-27 20:55:29 -0500

    start sanity

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,22 +4,41 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+// Load variables from `.env` as soon as possible
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+// require("dotenv").config()
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV || "development"}`,
+// })
+
 module.exports = {
   /* Your site config here */
-  siteMetadata:{
-    title: 'Simply Recipes',
-    description: 'Nice and clean recipes site',
-    author: 'Andre Jarboe',
-    person:{
-      name: 'jack', age: 35
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "Andre Jarboe",
+    person: {
+      name: "jack",
+      age: 35,
     },
     simpleData: ["item 1", "item 2"],
-    complexData:[
-      {name: "john", age:32},
-      {name: "Jessica", age:21}
-    ]
+    complexData: [
+      { name: "john", age: 32 },
+      { name: "Jessica", age: 21 },
+    ],
   },
   plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: process.env.SANITY_PROJECT_ID,
+        dataset: process.env.SANITY_DATASET,
+        // token: process.env.SANITY_TOKEN,
+      },
+    },
     "gatsby-plugin-postcss",
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
@@ -32,5 +51,4 @@ module.exports = {
       },
     },
   ],
-  
 }
